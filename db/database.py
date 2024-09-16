6f35a0ec5a9a4afd09870438475384501ebea9a0
Mai Long <130972390+C-Kang17@users.noreply.github.com>
2024-09-16 13:28:59 +0700

    Update database.py

--- db/database.py
+++ db/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 
 # Thay đổi các thông tin kết nối phù hợp với cấu hình của bạn
-DATABASE_URL = "oracle+cx_oracle://QL_SANBONGMINI:123@192.168.1.3:1521/orcl2"
+DATABASE_URL = "oracle+cx_oracle://QL_SANBONGMINI:123@localhost:1521/orcl2"
 
 engine = create_engine(DATABASE_URL, echo=True)
 
