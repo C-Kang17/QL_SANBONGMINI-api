14380bde1fba0a1c9ce8b206815685f317790c0f
TL17 <thanhlongm17@gmail.com>
2024-11-06 15:06:41 +0700

    fix config

---
+++ db/config.py
@@ -0,0 +1,8 @@
+DB_HOST = "localhost"  # Thay đổi IP khi cần thiết
+DB_PORT = "1521"
+DB_USER = "QL_SANBONGMINI"
+DB_PASS = "123"
+DB_SID = "orcl2"
+
+key = 7
+key_des = "Thats my Kung Fu"
--- module/v1/Users/config.py
+++
@@ -1,2 +0,0 @@
-key = 7
-key_des = "Thats my Kung Fu"
